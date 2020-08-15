f05e81abec5595ce3d36ac4d9150668245b4499f
AndreyS <endry.sh.83.v@gmail.com>
2020-08-15 21:10:32 +0300

    4 komit models

--- work/admin.py
+++ work/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Kontakt, Firma, kanban, businnesManager, goods, settings, documents, HeadMenu
+from .models import Kontakt, Firma, kanban, deal, goods, settings, documents, HeadMenu
+
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
+    list_display_links = ('title', 'content')
+    search_fields = ('first_name', 'first_name', 'age', 'r_r', 'companiName',)
 
 admin.site.register(Kontakt)
 admin.site.register(Firma)
 admin.site.register(kanban)
-admin.site.register(businnesManager)
+admin.site.register(deal)
 admin.site.register(goods)
 admin.site.register(settings)
 admin.site.register(documents)
